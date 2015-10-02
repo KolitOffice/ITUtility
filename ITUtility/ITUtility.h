c1135325e609b1db0b980994d19453a0804e27f7
ItsaraChai <itsaraph@ais.co.th>
2015-10-02 13:51:26 +0700

    Initial Commit

---
+++ ITUtility/ITUtility.h
@@ -0,0 +1,19 @@
+//
+//  ITUtility.h
+//  ITUtility
+//
+//  Created by Itsaraporn Chaichayanon on 10/2/15.
+//  Copyright © 2015 Itsaraporn Chaichayanon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ITUtility.
+FOUNDATION_EXPORT double ITUtilityVersionNumber;
+
+//! Project version string for ITUtility.
+FOUNDATION_EXPORT const unsigned char ITUtilityVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ITUtility/PublicHeader.h>
+
+
